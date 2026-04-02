f4ce11616c09a758908c10f068d94516f675dcec
wisteria0793 <wisteria07930791@gmail.com>
2026-04-02 21:50:31 +0900

    Fix backend startup error by correcting WSGI settings and using --chdir in gunicorn

--- myproject/guesthouse_tomoe/guesthouse_tomoe/wsgi.py
+++ myproject/guesthouse_tomoe/guesthouse_tomoe/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guesthouse_tomoe.guesthouse_tomoe.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'guesthouse_tomoe.settings')
 
 application = get_wsgi_application()
